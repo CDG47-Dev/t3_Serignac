4de905cc28db8d651c9337ca57351aa157dadbce
Chediard <christophe.hediard@outlook.fr>
2016-06-10 10:29:44 +0200

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,6 +2,8 @@
 if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
-
+// Add BackendLayouts BackendLayouts for the BackendLayout DataProvider
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTS/Mod/WebLayout/BackendLayouts.txt">');
 
+// TCEFORM
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTS/TCEFORM.txt">');
